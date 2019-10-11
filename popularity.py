34dc421e580a2b0f7739e08b736061d3644c2002
Nedu Robert <nedumrobert@gmail.com>
2019-10-11 12:20:03 +0100

    fix: fixed breaking issue

--- popularity.py
+++ popularity.py
@@ -1,30 +1,32 @@
+"""
+This script saves a popularity recommender model
+"""
 try:
     import pandas as pd
     import joblib
     from sklearn.model_selection import train_test_split
     import rec
 
-except ImportError as e:
-    print(e)
+except ImportError as import_error:
+    print(import_error)
+
 
 def get_data():
     '''
     Function to get data from file.
-    
+
     Returns
     (Lists): Notifications table
     '''
-    #Read the notifications table
-
+    # pylint: disable=line-too-long,locally-disabled
     notifications = pd.read_csv('https://raw.githubusercontent.com/pibeebeks/task-user-recommender/master/data/notifications.csv')
     notifications.drop(['id', 'created_at', 'updated_at'], axis=1, inplace=True)
     return notifications
 
-
 def save_model(recommender):
     '''
-    Function to save recommended output to a file 
-    
+    Function to save recommended output to a file
+
     Parameters
     recommender
     (file):a .py file of the reccommender model
@@ -34,27 +36,27 @@ def save_model(recommender):
     '''
     filename = 'popular.sav'
     joblib.dump(recommender, filename)
+    print('Model successfully saved in popular.sav')
 
 def main():
     '''
-    Function to define main 
-    
+    Function to define main
+
     Returns
     ():
     '''
     try:
         notifications = get_data()
         train_data, test_data = train_test_split(notifications, test_size=0.40, random_state=0)
-        
-        recommender = rec.popularity_recommender_py()
+
+        recommender = rec.PopularityRecommender()
         recommender.create(train_data, 'user_id')
         recommender.create(test_data, 'user_id')
 
         save_model(recommender)
-    
-    except (NameError, ValueError) as e:
-        print("Error saving model",e)
 
+    except (NameError, ValueError) as error:
+        print("Error saving model", error)
 
 if __name__ == '__main__':
     main()
